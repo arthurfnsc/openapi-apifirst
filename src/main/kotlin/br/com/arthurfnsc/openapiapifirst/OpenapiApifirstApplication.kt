d41e7e14403fac7b08e6190dfed6619b8527f737
Arthur Magalhaes Fonseca <arthurfnsc@gmail.com>
2020-02-29 20:53:54 -0300

    :sparkles: Adiciona suporte a Gradle  (#1)
    Adiciona suporte a Gradle em projeto

---
+++ src/main/kotlin/br/com/arthurfnsc/openapiapifirst/OpenapiApifirstApplication.kt
@@ -0,0 +1,11 @@
+package br.com.arthurfnsc.openapiapifirst
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class OpenapiApifirstApplication
+
+fun main(args: Array<String>) {
+  runApplication<OpenapiApifirstApplication>(*args)
+}
+
